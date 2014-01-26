ca2ec1d997886d4a521b26c872c9dade711a4942
EpicWolverine <epicwolverine@me.com>
2014-01-26 15:58:11 -0500

    Update to reflect project name change.

--- blueracer.py
+++ blueracer.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python -tt
 # Copyright 2014 Brendan Ferracciolo
 # 
-# This file is part of EmailComics.
+# This file is part of BlueRacer.
 #
-# EmailComics is free software: you can redistribute it and/or modify
+# BlueRacer is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# EmailComics is distributed in the hope that it will be useful,
+# BlueRacer is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with EmailComics. If not, see <http://www.gnu.org/licenses/>.
+# along with BlueRacer. If not, see <http://www.gnu.org/licenses/>.
 
 
 import sys
