3f5863eea5029086ec75471942c400f259c8281a
EpicWolverine <epicwolverine@me.com>
2014-01-26 18:48:05 -0500

    Added email support; may work with SMTP server installed.

--- blueracer.py
+++ blueracer.py
@@ -16,18 +16,21 @@
 # You should have received a copy of the GNU General Public License
 # along with BlueRacer. If not, see <http://www.gnu.org/licenses/>.
 
+"""<insert man page here>"""
 
-import sys
-import urllib2
-import re
+import sys          # for various I/O functions
+import urllib2      # URL fetching and handleing
+import re           # regular expression support
+import smtplib      # email sending support
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 def main():
-    wget(sys.argv[1])
+    sendEmail(sys.argv[3], "test@example.com", sys.argv[2], wget(sys.argv[1]))
 
     
 def wget(url):
     try:
-        
         page = urllib2.urlopen(urllib2.Request(url,headers={'User-Agent': 'Mozilla/5.0'}))
         #print page.read()
         
@@ -35,12 +38,57 @@ def wget(url):
             comic = re.search(r'<meta\sname="twitter:image"\scontent="([\w\s\W]+?)"\s/>', page.read())
             if comic:
                 print comic.group(1)
+                return comic.group(1)
             else:
-                print comic
+                print "Comic image not found."
+                return
         else:
             print "This website is not supported."
+            return
     except IOError:
         print 'problem reading url:', url
+        
+def sendEmail(receiver, sender, comicName, imgURL):
+    # Create message container - the correct MIME type is multipart/alternative.
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = comicName + "for %A, %B %d, %Y"
+    msg['From'] = sender
+    msg['To'] = receiver
+
+    # Create the body of the message (a plain-text and an HTML version).
+    text = comicName + "for %A, %B %d, %Y\n" + imgURL + "\n\nSent with BlueRacer: https://github.com/EpicWolverine/BlueRacer"
+    html = """\
+    <html>
+      <head></head>
+      <body>
+        <center>
+            <p>
+               <h1>""" + comicName + """for %A, %B %d, %Y<br>
+               <img src=\"""" + imgURL + """\" alt=\"""" + imgURL + """\"></img><br>
+               <br>
+               Sent with <a href=\"https://github.com/EpicWolverine/BlueRacer\">BlueRacer</a>.
+            </p>
+        </center>
+      </body>
+    </html>
+    """
+    
+    # Record the MIME types of both parts - text/plain and text/html.
+    part1 = MIMEText(text, 'plain')
+    part2 = MIMEText(html, 'html')
+
+    # Attach parts into message container.
+    # According to RFC 2046, the last part of a multipart message, in this case
+    # the HTML message, is best and preferred.
+    msg.attach(part1)
+    msg.attach(part2)
+
+    # Send the message via local SMTP server.
+    s = smtplib.SMTP('localhost')
+    # sendmail function takes 3 arguments: sender's address, recipient's address
+    # and message to send - here it is sent as one string.
+    s.sendmail(sender, receiver, msg.as_string())
+    s.quit()
 
 # This is the standard boilerplate that calls the main() function.
 if __name__ == '__main__':
